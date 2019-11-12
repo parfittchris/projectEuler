737d89879a5ff31da0e5e9a90d8a39332038bbd0
Chris Parfitt <parfitt.chris7@gmail.com>
2019-11-12 09:09:49 -0500

    eueler 14

--- 13_largeSum.js
+++ 13_largeSum.js
@@ -1,4 +1,6 @@
+// https://projecteuler.net/problem=13
 
+// Work out the first ten digits of the sum of the following one - hundred 50 - digit numbers.
 
 
 let array = [37107287533902102798797998220837590246510135740250,
